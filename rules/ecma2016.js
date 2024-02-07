74fdec5763c83825da8dbf0c5d1f7c3de6b06380
xujiujiu <906784584@qq.com>
2024-02-07 16:29:43 +0800

    ecma2016语法兼容性数据

--- rules/ecma2016.js
+++ rules/ecma2016.js
@@ -1,29 +1,62 @@
+const BrowserMap = {
+  BinaryExpression: {
+    browsers: {
+      chrome: 52,
+      firefox: 52,
+      safari: 10.1,
+      opera: 39,
+      edge: 14,
+      ie: 11,
+      ios: 10.3,
+      android: 52,
+    },
+    message: "Using BinaryExpression operator ** is not allowed",
+  },
+  AssignmentExpression: {
+    browsers: {
+      chrome: 52,
+      firefox: 52,
+      safari: 10.1,
+      opera: 39,
+      edge: 14,
+      ie: 11,
+      ios: 10.3,
+      android: 52,
+    },
+    message: "Using AssignmentExpression operator **= is not allowed",
+  },
+};
 module.exports = function (usePlugin) {
   return {
     meta: {
       docs: {
-        description: 'ecma2016 rules'
-      }
+        description: "ecma2016 rules",
+      },
     },
-    create (context) {
+    create(context) {
       return {
-        BinaryExpression (node) {
-          if (node.operator === '**' && usePlugin('exponentiation-operator')) {
+        BinaryExpression(node) {
+          if (node.operator === "**" && usePlugin("exponentiation-operator")) {
+            const { browsers, message } = BrowserMap["BinaryExpression"];
             context.report({
+              type: 'BinaryExpression',
               node,
-              message: 'Using BinaryExpression operator ** is not allowed'
-            })
+              browsers,
+              message,
+            });
           }
         },
-        AssignmentExpression (node) {
-          if (node.operator === '**=' && usePlugin('exponentiation-operator')) {
+        AssignmentExpression(node) {
+          if (node.operator === "**=" && usePlugin("exponentiation-operator")) {
+            const { browsers, message } = BrowserMap["AssignmentExpression"];
             context.report({
               node,
-              message: 'Using AssignmentExpression operator **= is not allowed'
-            })
+              browsers,
+              message,
+            });
           }
-        }
-      }
-    }
-  }
-}
+        },
+      };
+    },
+  };
+};
