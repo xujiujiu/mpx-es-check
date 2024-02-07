3d564db3a485f0d8819431f794e3533d3c6467bc
xujiujiu <906784584@qq.com>
2024-02-07 17:26:06 +0800

    ecma2019兼容性数据

--- rules/ecma2019.js
+++ rules/ecma2019.js
@@ -1,21 +1,40 @@
+const BrowserMap = {
+  CatchClause: {
+    browsers: {
+      chrome: "66",
+      firefox: "58",
+      opera: "53",
+      safari: "11.1",
+      edge: "79",
+      ios: "11.3",
+      android: "66",
+    },
+    message:
+      "Using CatchClause param is null is not allowed, E.g., try { foo() } catch { bar() }",
+  },
+};
+
 module.exports = function (usePlugin) {
   return {
     meta: {
       docs: {
-        description: 'ecma2019 rules'
-      }
+        description: "ecma2019 rules",
+      },
     },
-    create (context) {
+    create(context) {
       return {
-        CatchClause (node) {
-          if (node.param === null && usePlugin('optional-catch-binding')) {
+        CatchClause(node) {
+          if (node.param === null && usePlugin("optional-catch-binding")) {
+            const { browsers, message } = BrowserMap["CatchClause"];
             context.report({
+              type: 'CatchClause',
               node,
-              message: 'Using CatchClause param is null is not allowed, E.g., try { foo() } catch { bar() }'
-            })
+              browsers,
+              message,
+            });
           }
-        }
-      }
-    }
-  }
-}
+        },
+      };
+    },
+  };
+};
