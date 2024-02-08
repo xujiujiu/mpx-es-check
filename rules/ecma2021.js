20b801c253287cfdf227b5937c7505b16c5fa39e
xujiujiu <906784584@qq.com>
2024-02-08 09:42:52 +0800

    ecma2021兼容性数据

--- rules/ecma2021.js
+++ rules/ecma2021.js
@@ -1,28 +1,50 @@
+const BrowserMap = {
+  AssignmentExpression: {
+    browsers: {
+      chrome: "85",
+      firefox: "85",
+      opera: "71",
+      safari: "14",
+      edge: "85",
+      ios: "14",
+      android: "85",
+    },
+    message: "使用的赋值运算符 ${type} 浏览器暂不支持，需要走 babel 转译",
+  },
+};
+
 module.exports = function (usePlugin) {
   return {
     meta: {
       docs: {
-        description: 'ecma2021 rules'
-      }
+        description: "ecma2021 rules",
+      },
     },
-    create (context) {
+    create(context) {
       return {
         /**
          * AssignmentExpression node has short-circuiting behavior if the operator property is any of "||=","&&=", and "??=".
          * @param node
          * @constructor
          */
-        AssignmentExpression (node) {
-          if (node.operator === '||=' || node.operator === '??=' || node.operator === '&&=') {
-            if (usePlugin('logical-assignment-operators')) {
+        AssignmentExpression(node) {
+          if (
+            node.operator === "||=" ||
+            node.operator === "??=" ||
+            node.operator === "&&="
+          ) {
+            if (usePlugin("logical-assignment-operators")) {
+              const { browsers, message } = BrowserMap["AssignmentExpression"];
               context.report({
+                type: 'AssignmentExpression',
                 node,
-                message: `使用的赋值运算符 ${node.operator} 浏览器暂不支持，需要走 babel 转译`
-              })
+                browsers,
+                message: message.replace('${type}', node.operator),
+              });
             }
           }
-        }
-      }
-    }
-  }
-}
+        },
+      };
+    },
+  };
+};
